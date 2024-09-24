effae3fb00a523261cf03fab6db8d184a49b92c7
Md Rafidul Islam <rafidulemon@gmail.com>
2024-09-24 21:26:43 +0600

    prisma build on package

--- app/components/about-details/Personal-details.tsx
+++ app/components/about-details/Personal-details.tsx
@@ -58,7 +58,10 @@ function PersonalDetails() {
           isTransparentBG
         />
       </div>
-      <Grid colCount={2} className="gap-4 md:gap-6 lg:gap-7 xl:gap-8 text-white text-[12px] md:text-[14px] lg:text-[15px] xl:text-[16px]">
+      <Grid
+        colCount={2}
+        className="gap-4 md:gap-6 lg:gap-7 xl:gap-8 text-white text-[12px] md:text-[14px] lg:text-[15px] xl:text-[16px]"
+      >
         <div>
           <Text text="Name " isBold />
         </div>
@@ -87,7 +90,8 @@ function PersonalDetails() {
           <Text text="Date of birth " isBold />
         </div>
         <div>
-          <Text text={new Date(userData.dob).toLocaleDateString()} /> {/* Format date if needed */}
+          <Text text={new Date(userData.dob).toLocaleDateString()} />{" "}
+          {/* Format date if needed */}
         </div>
         <div>
           <Text text="Nationality " isBold />
@@ -105,7 +109,12 @@ function PersonalDetails() {
           <Text text="Languages" isBold />
         </div>
         <div>
-          <Text text={userData.languages.map(lang => lang.language_name).join(", ")} /> {/* Display languages */}
+          <Text
+            text={userData.languages
+              .map((lang) => lang.language_name)
+              .join(", ")}
+          />{" "}
+          {/* Display languages */}
         </div>
       </Grid>
     </div>
